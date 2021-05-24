d48e33b3569d87d7ddb58654ae3df1e1dc543345
Ranjit <ranjitkumar448@yahoo.com>
2021-05-25 05:18:38 +0530

    added header

--- src/app/shared/components/shared/header/header.component.ts
+++ src/app/shared/components/shared/header/header.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+
+export interface MENUITEM{
+  label:string,
+  icon:string,
+  active:boolean
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('navbarMenu') navbarMenu!: ElementRef;
+  links:MENUITEM[] =[
+    {
+      label:"Contact",
+      icon:"ri-contacts-line icon",
+      active:false
+    },
+    {
+      label:"Donate",
+      icon:"ri-home-line icon",
+      active:false
+    },
+    {
+      label:"Home",
+      icon:"ri-home-line icon",
+      active:true
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleNav() {
+    if (this.navbarMenu.nativeElement.classList.contains('responsive')) {
+      this.navbarMenu.nativeElement.classList.remove('responsive');
+    } else {
+      this.navbarMenu.nativeElement.classList.add('responsive');
+    }
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
