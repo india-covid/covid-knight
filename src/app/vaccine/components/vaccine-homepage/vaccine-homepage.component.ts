44c4b603a83b959bdf52fbd1cf57978f4345381b
Ranjit <ranjitkumar448@yahoo.com>
2021-06-02 23:47:53 +0530

    fixing style

--- src/app/vaccine/components/vaccine-homepage/vaccine-homepage.component.ts
+++ src/app/vaccine/components/vaccine-homepage/vaccine-homepage.component.ts
@@ -1,11 +1,10 @@
 import { User } from './../../../core/models/user.model';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/core/auth.service';
 import { VaccineRestService } from '../../services/vaccine-rest.service';
-
 @Component({
   selector: 'app-vaccine-homepage',
   templateUrl: './vaccine-homepage.component.html',
@@ -14,10 +13,13 @@ import { VaccineRestService } from '../../services/vaccine-rest.service';
 export class VaccineHomepageComponent implements  OnDestroy {
 
   authSub: Subscription;
+  heightVal=0;
+  @ViewChild('infoWrapper') infoWrapper:ElementRef|null=null;
   constructor(
     private authService: AuthService,
     private spinner: NgxSpinnerService,
-    private router: Router) {
+    private router: Router,
+    private renderer:Renderer2) {
     this.authSub = this.authService.user$.subscribe((user) => {
       if (user && user.phoneNumber) {
         this.router.navigate(["/home"])
@@ -32,5 +34,15 @@ export class VaccineHomepageComponent implements  OnDestroy {
 
   ngOnDestroy() {
     this.authSub.unsubscribe();
+  }
+  resetHeight(){
+
+    this.renderer.setStyle(this.infoWrapper?.nativeElement, 'height', (window.innerHeight-80)+"px");
+    console.log(this.infoWrapper?.nativeElement,window.innerHeight);
+  }
+
+  ngAfterViewInit() {
+    // window.addEventListener("resize", this.resetHeight);
+   this.resetHeight();
   }
 }
