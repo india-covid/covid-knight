39c8b1f49d0ed9ed3f7383ee5ac2aea711f9e92c
ChandanWarad <cwarad@aitheon.com>
2021-06-19 01:19:41 +0530

    fix: added href links and removed unwanted spaces

--- src/app/vaccine/components/vaccine-faq/vaccine-faq.component.ts
+++ src/app/vaccine/components/vaccine-faq/vaccine-faq.component.ts
@@ -33,22 +33,22 @@ export class VaccineFaqComponent implements OnInit {
       {
         question: 'What about privacy?',
         answer:
-          'We do not collect any other information other than WhatsApp number. Phone numbers are automatically deleted after the notification subscription expires. We do not run any kind of ads. You can support us by donating,',
+          `We do not collect any other information other than WhatsApp number. Phone numbers are automatically deleted after the notification subscription expires. We do not run any kind of ads. You can support us by <a href="https://vaccine.india-covid.info/#donate" target="_blank">donating</a>.`,
       },
       {
         question: 'Can I Stop the notifications?',
         answer:
-          'Yes, you can stop the notification anytime by visiting this website',
+          `Yes, you can stop the notification anytime by visiting this website.`,
       },
       {
         question: 'Can I check vaccine availability on this website?',
         answer:
-          'Yes, you can check Vaccine availability on this website for the entire India. Data is synced real time with Cowin. We use Cowin’s public API.',
+          `Yes, you can check Vaccine availability on this website for the entire India. Data is synced real time with Cowin. We use Cowin’s <a href="https://apisetu.gov.in/public/marketplace/api/cowin" target="_blank">public API</a>.`,
       },
       {
         question: 'Can I book vaccine from this website?',
         answer:
-          'No, Currently we only send notifications and show availability. Please visit Cowin to book appointments.',
+          'No, Currently we only send notifications and show availability. Please visit <a href="https://www.cowin.gov.in/" target="_blank">Co-WIN</a> to book appointments.',
       },
     ];
   }
