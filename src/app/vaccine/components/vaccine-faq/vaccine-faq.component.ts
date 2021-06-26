be9ca27374dff0972ddf565642f9edc8c2a7931d
Rahul <rshinde@aitheon.com>
2021-06-26 14:50:13 +0530

    added another faq

--- src/app/vaccine/components/vaccine-faq/vaccine-faq.component.ts
+++ src/app/vaccine/components/vaccine-faq/vaccine-faq.component.ts
@@ -49,6 +49,11 @@ export class VaccineFaqComponent implements OnInit {
           `We do not collect any information other than WhatsApp number. Phone numbers are automatically deleted after the notification subscriptions expire. We do not run any kind of ads. You can support us by <a href="${paymentUrl}" target="_blank">donating</a>.`,
       },
       {
+        question: 'Why should I use Covid Knight?',
+        answer:
+          `Covid Knight is blazing fast hybrid app, it's built for low end devices. We also send WhatsApp notifications.`,
+      },
+      {
         question: 'How do I subscribe to WhatsApp notifications?',
         answer:
           'Click on add centers, select your area, select all the the centers you want to subscribe to and click subscribe.',
@@ -56,15 +61,15 @@ export class VaccineFaqComponent implements OnInit {
       {
         question: 'Can I Stop the notifications?',
         answer:
-          `Yes, you can stop the notification anytime by visiting this website.`,
+          `Yes, you can stop the notification anytime by visiting this website/app.`,
       },
       {
-        question: 'Can I check vaccine availability on this website?',
+        question: 'Can I check vaccine availability on this website/app?',
         answer:
-          `Yes, you can check Vaccine availability on this website for the entire India. Data is synced real time with Cowin. We use Cowin’s <a href="https://apisetu.gov.in/public/marketplace/api/cowin" target="_blank">public API</a>.`,
+          `Yes, you can check Vaccine availability on this website/app for the entire India. Data is synced real time with Cowin. We use Cowin’s <a href="https://apisetu.gov.in/public/marketplace/api/cowin" target="_blank">public API</a>.`,
       },
       {
-        question: 'Can I book vaccine from this website?',
+        question: 'Can I book vaccine from this website/app?',
         answer:
           'No, Currently we only send notifications and show availability. Please visit <a href="https://www.cowin.gov.in/" target="_blank">Co-WIN</a> to book appointments.',
       },
